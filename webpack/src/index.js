13f73dbee47c540960d67bbb797d6b563a76311a
wanghuan <196912334@qq.com>
2019-05-15 17:34:40 +0800

    update

---
+++ webpack/src/index.js
@@ -0,0 +1,3 @@
+console.log('wanghuan')
+
+require('./index.css')
---
+++ webpack/webpack.config.js
@@ -0,0 +1,79 @@
+// 内置模块path
+let path = require('path')
+// html模板插件
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let MiniCssExtractPlugin = require('mini-css-extract-plugin')  //CSS插件
+let OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+	// devServer: {   //不配置的情况默认为8080
+	// 	port: 3000,
+	// 	progress: true,
+	// 	contentBase: './build',
+	// 	compress: true
+	// },
+	optimization: { // 优化项
+		minimizer: [
+			new OptimizeCssAssetsWebpackPlugin()
+	},
+	mode: 'development', // 开发模式，生产模式production
+	entry: './src/index.js', // 入口
+	output: {
+		filename: 'bundle.js', // 打包后文件名，用hash区别文件名"bundle.[hash:8].js"
+		path: path.resolve(__dirname, 'dist'), // 打包后文件路径，绝对路径
+	},
+	// 所有的webpack插件
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: 'index.html',
+			// minify: { // 压缩配置
+			// 	removeAttributeQuotes: true, // 删除属性双引号
+			// 	collapseWhitespace: true, // 折叠空行
+			// },
+			// hash: true // 哈希戳
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'main.css'
+		})
+	],
+	module: {
+		rules: [
+			// loader 特点单一；字符串定义一个loader或对象（可传入参数options）; 多个loader使用数组 ; loader 顺序，默认从右向左执行
+			{
+				test: /\.css$/,
+				use: [{
+						loader: 'style-loader', // style-loader 把 css 插入到head标签中
+						options: {
+							insertAt: 'top' // style标签内样式放在最上面
+						}
+					},
+					'postcss-loader',
+					'css-loader', // css-loader 解析 @import 这个语法
+					
+				]
+			},
+			{
+				test: /\.less$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					'postcss-loader',
+					'css-loader',
+					'less-loader',
+				]
+			},
+			{
+				test: /\.(gif|jpg|jpeg|png|svg)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							limit: 1024,
+							name: '[name]-test.[ext]'
+						}
+					}
+				]
+			}
+		]
+	}
+}
